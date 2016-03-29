b6f788d58f0f2b3ef7d8ce0208f19e0ec6d6baec
gracie lundell and paul hess <gracielundell@gmail.com>
2016-03-29 11:59:48 -0700

    add new article functionality

---
+++ app/components/bustle-admin.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    savePost1() {
+      var params = {
+        headline: this.get("headline"),
+        photo: this.get("photo"),
+        author: this.get("author"),
+        article: this.get("article"),
+      };
+      this.sendAction('savePost2', params);
+    }
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend({
   model(){
     return this.store.findAll('bustle');
   },
+
+  actions: {
+    savePost3(params) {
+      var newArticle = this.store.createRecord('bustle', params);
+      newArticle.save();
+      this.transitionTo('index');
+    }
+  }
 });
